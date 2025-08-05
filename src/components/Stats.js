47baa0584a8380d34416a8efa87c0878ee874077
Thanh Nguyễn <thanhnguyen20051457@gmail.com>
2025-08-05 11:58:18 +0700

    Add files via upload

---
+++ src/components/Stats.js
@@ -0,0 +1,22 @@
+export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numberPacked = items.filter((item) => item.packed).length;
+  const percentages = Math.round((numberPacked / numItems) * 100);
+  return (
+    <footer className="stats">
+      <em>
+        {percentages === 100
+          ? ` You got everything! Ready to go ✈️`
+          : ` 🧳You have ${numItems} items on your list, and you already packed
+${numberPacked} (${percentages}%)`}
+      </em>
+    </footer>
+  );
+}
